d092d875d0a3f444b4b57657b2c6d160ae38cb55
Tamer0508 <dengelov.tamerlan.01@gmail.com>
2026-05-04 19:45:24 +0600

    fix: regenerate prisma client with appUserId

---
+++ server/prisma/migrations/20260504134458_fix_app_user_id/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "session" (
+    "sid" VARCHAR NOT NULL,
+    "sess" JSON NOT NULL,
+    "expire" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "session_pkey" PRIMARY KEY ("sid")
+);
